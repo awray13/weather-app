a9c3bb97f9675defbb6624d21927c81ee2ea1edd
Ashton Wray <aricks1986@gmail.com>
2019-03-05 23:45:10 -0500

    updated weather.js, titles.js, and form.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-const Weather = () => {
-  return (
-    <div>
-      { this.props.city && this.props.country && <p>Location: { this.props.city }, { this.props.country }</p> }
+const Weather = props => (
+  <div>
+      { props.city && props.country && <p>Location: { props.city }, { props.country }</p> }
 
-      { this.props.temperature && <p>Temperature: { this.props.temperature }</p> }
+      { props.temperature && <p>Temperature: { props.temperature }</p> }
 
-      { this.props.humidity && <p>Humidity: { this.props.humidity }</p> }
+      { props.humidity && <p>Humidity: { props.humidity }</p> }
 
-      { this.props.description && <p>Conditions: { this.props.description }</p> }
+      { props.description && <p>Conditions: { props.description }</p> }
 
-      { this.props.error && <p>{ this.props.error }</p> }
+      { props.error && <p>{ props.error }</p> }
     </div>
-  );
-}
+);
 
 export default Weather;
